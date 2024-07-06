ccf99316eb728934185aad542416960707ed6ad1
midhundas27 <dasmidhun27@gmail.com>
2024-07-06 19:48:04 +0530

    header made responsive

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './header.css';
 
 function Header() {
+  const [menuActive, setMenuActive] = useState(false);
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <div className='header-container'>
-      <div className="header-website-logo">
+      <div className="header-website-logo-box">
         <Link to="/">
-          <img src="header-image.png" alt="company-logo"></img>
+          <img src="header-image.png" alt="company-logo" className="header-website-logo"></img>
         </Link>
       </div>
       <div className="header-buttons">
@@ -15,6 +20,15 @@ function Header() {
         <Link to="#services"><div className="header-buttons-click">Services</div></Link>
         <Link to="#about"><div className="header-buttons-click">About</div></Link>
         <Link to="#contact"><div className="header-buttons-click">Contact</div></Link>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div>&#9776;</div>
+      </div>
+      <div className={`menu ${menuActive ? 'active' : ''}`}>
+        <Link to="/" onClick={toggleMenu}><div className="header-buttons-click">Home</div></Link>
+        <Link to="#services" onClick={toggleMenu}><div className="header-buttons-click">Services</div></Link>
+        <Link to="#about" onClick={toggleMenu}><div className="header-buttons-click">About</div></Link>
+        <Link to="#contact" onClick={toggleMenu}><div className="header-buttons-click">Contact</div></Link>
       </div>
     </div> 
   );
